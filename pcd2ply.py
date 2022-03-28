517d97cb8e8eac35fd41c618f31b39d65df2b1f9
Tiffany Zhou <xueying.zhou@dorabot.com>
2022-03-28 17:45:14 +0800

    add ply2pcd and pcd2ply

--- pcd2ply.py
+++ pcd2ply.py
@@ -3,8 +3,7 @@ import sys
 
 path = str(sys.argv[1])
 pcd = o3d.io.read_point_cloud(path)
-# visualization.draw_geometries([pcd])
 
-new_path = path[:-3] + "pcd"
+new_path = path[:-3] + "ply"
 print(path, " -> ", new_path)
 o3d.io.write_point_cloud(new_path, pcd)
